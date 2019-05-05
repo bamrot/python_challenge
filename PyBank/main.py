9cf0e15f1addf603647d98d09d1ba19dee4b4441
bamrot <bamrot@gmail.com>
2019-05-04 23:23:00 -0500

    homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,22 +1,55 @@
 import os
 import csv
-
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-### if it worked out it should have looked like 
-with open(csvpath, newline='') as csvfile:
+data=[]
+profit_loss=[]
+list_profit_loss = []
+previous_value = 0
 
-    ## CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile, delimiter=',')
-    total_months = 0
-    next(csvreader)
+#opening
+with open(csvpath,newline="") as csvfile:
+    csvreader = csv.reader(csvfile,delimiter=",")
+    header = next(csvreader)
+    record = list(csvreader)
+    total_months = len(record)
 
-    for row in csvreader:
-        print(row)
-        total_months = 1+total_months
+    for row in record:
+        amount = row[1]
+        profit_loss.append(int(amount))
+        profit_loss_gap=int(row[1])-previous_value
+        previous_value=int(row[1])
+        list_profit_loss.append(profit_loss_gap)
+    final_list=list_profit_loss[1:]
+    #results
+    Total=sum(profit_loss)
+    Average=sum(final_list)/len(final_list)
+    increase=max(final_list)
+    decrease=min(final_list)
+
+#printing
+output=(f'''financial analysis
+------------------
+Total months:{total_months}
+Total:{Total}
+Average change:{Average}
+Greatest increase:{increase}
+Greatest decrease:{decrease}''')
+
+print(output)
+
+with open("output.txt",'w') as file:
+    file.write(output)
     
-    print(total_months)
 
+
+
+
+    
+    
+
+
+   
 
 
 
